fa35fbad3a5063fa646f182f9670d3a517a9dca0
PSC2313 <pedrosantoscintra07@gmail.com>
2025-11-19 15:05:08 -0300

    exemplo2

---
+++ aula4/exemplo2.js
@@ -0,0 +1,12 @@
+const imput = require("../input");
+
+(async () => {
+    while (num != 7) {
+        console.log("Digite um numero!");
+        var num = Number(await imput());
+
+        if (num == 7) {
+            console.log("Acertou!!");
+        }
+    }
+})();   
