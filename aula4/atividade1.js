09090563358ae4ee7589172ea664396b02c1b748
PSC2313 <pedrosantoscintra07@gmail.com>
2025-11-24 14:21:07 -0300

    aula5

--- aula4/atividade1.js
+++ aula4/atividade1.js
@@ -1,19 +1,18 @@
 const imput = require("../input");
 
 (async () => {
-    let num;
-    let aleatorio;
+    let num = 0;
+    let aleatorio = -1;
     for (i = 10; i >= 0 && num != aleatorio ; i--) {
         console.log("Digite um numero:");
         num = Number(await imput());
         aleatorio = Math.floor(Math.random() * 10);
         console.log("numero sorteado " + aleatorio);
         console.log();
-    } 
-    if (num != aleatorio) {
-        console.log("acertou!!");
-    } else {
-        console.log("Você errou!!");
         console.log(`Você tem ${i} tentativas`);
+    } if (num != aleatorio) {
+        console.log("Errou!!");
+    } else {
+        console.log("Acertou!!");
     }
 })();
---
+++ aula5/exemplo1.js
@@ -0,0 +1,13 @@
+// Quando eu inicio a variavel com [] estou declarando um vetor (lista ou array)
+let vetor = [];
+
+//0 É a primeira posição, indice 0
+vetor[0] = "livro";
+
+let pacote = "Salgadinhos";
+let nome = "Pedro";
+vetor[1] = pacote;
+vetor[1] = nome;
+
+console.log(vetor);// ['livro', 'Pedro']
+console.log(vetor[1]);// 'Pedro'
